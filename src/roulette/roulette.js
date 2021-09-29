a2f4df0f0ccae5853c71e122d0b1804d86988208
Claire MATHIRON <claire@MacBook-Pro-de-Claire.local>
2021-09-29 14:38:25 +0200

    modif page rroulette

--- src/roulette/roulette.js
+++ src/roulette/roulette.js
@@ -1,3 +1,13 @@
 function displayRandomResto(e) {
     randResto = resto[Math.floor(Math.random() * restoArray.length)];
 }
+
+// let bread_crumbs=
+
+var roulette = document.getElementById("roulette")
+console.log(roulette)
+roulette.addEventListener("click", trigerRoulette)
+
+function trigerRoulette(e){
+roulette.classList.add("element-animation")
+}
